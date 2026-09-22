c4425e9636641fe07485f4d10eabd563f88d4b73
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2026-09-23 01:07:58 +0200

    aggiunta sezione formazione

--- script.js
+++ script.js
@@ -37,8 +37,8 @@ window.addEventListener('scroll', () => {
   if (!heroVisual) return;
   const y = Math.min(window.scrollY, 600);
   const isMobile = window.innerWidth < 801;
-  const translateFactor = isMobile ? .08 : .2;
-  const rotationFactor = isMobile ? .2 : .3;
+  const translateFactor = isMobile ? .12 : .2;
+  const rotationFactor = isMobile ? .3 : .3;
   heroVisual.style.transform = `translateY(${y * translateFactor}px) perspective(1000px) rotateY(${y * rotationFactor}deg)`;
 }, {passive:true});
 
